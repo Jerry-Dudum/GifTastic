7bd2d4017d39fb6af96b557946ab027e4b2bbb01
Jerry Dudum <jsdudum@gmail.com>
2019-09-11 18:15:13 -0700

    added functionality to js file so when a gif button is pressed gifs and they're ratings are displayed just ned to fix how they get displayed

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -9,7 +9,7 @@ function renderButtons() {
     for (var i = 0; i < gifs.length; i++) {
 
         var newGifButton = $("<button>");
-        newGifButton.addClass("gif");
+        newGifButton.addClass("gif-button");
         newGifButton.attr("data-name", gifs[i]);
         newGifButton.text(gifs[i]);
         $("#gif-buttons").append(newGifButton);
@@ -31,7 +31,7 @@ $("#add-gif").on("click", function (event) {
 });
 
 //this on click function will call on the function displayGif that will append all the gifs to the page
-$(document).on("click", ".gif", displayGif);
+$(document).on("click", ".gif-button", displayGif);
 
 //this gif will reach the giphy api with ajax and append gifs to the page
 function displayGif() {
@@ -46,6 +46,26 @@ function displayGif() {
     }).then(function (response) {
 
         console.log(response);
+
+        var results = response.data;
+
+        for (var i = 0; i < results.length; i++){
+            
+            var newGif = $("<div>");
+
+            var gifRating = $("<p>").text("Rating: " + results[i].rating);
+            var gifImage = $("<img>");
+            gifImage.attr("src", results[i].images.fixed_height_still.url);
+            gifImage.attr("data-still", results[i].images.fixed_height_still.url);
+            gifImage.attr("data-animate", results[i].images.fixed_height.url);
+            gifImage.attr("data-state", "still");
+            gifImage.addClass("gif");
+
+            newGif.append(gifRating);
+            newGif.append(gifImage);
+
+            $("#gifs-here").append(newGif);
+        }
 
     });
 }
